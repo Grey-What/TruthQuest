892b29687dfbbcdfd039dfb700f0c6e8d3576fe8
Grey <greywhat2002@gmail.com>
2024-06-25 12:58:16 +0200

    database works

---
+++ web_flask/db_test.py
@@ -0,0 +1,16 @@
+from app import app, db, User
+
+with app.app_context():
+    #db.create_all()
+    #db.session.add(User(username="test", email="test@localhost", password="test"))
+    #db.session.add(User(username="test2", email="test2@localhost", password="test2"))
+    #db.session.commit()
+
+    #db.drop_all()
+
+
+
+"""     a_user = User.query.filter_by(username="test").all()"""
+
+
+
