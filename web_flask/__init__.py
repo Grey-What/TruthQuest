0757a93474e8cf7f1c7d6c000038c9bcdac83a9b
Grey <greywhat2002@gmail.com>
2024-07-04 16:38:44 +0200

    changed to database

--- web_flask/__init__.py
+++ web_flask/__init__.py
@@ -6,8 +6,8 @@ from flask_login import LoginManager
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '7ea0b663144af24c7d5dff519b97ce54'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-"""app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://TQ_Admin:TruthQuest24@localhost/TruthQuest_db?auth_plugin=mysql_native_password'"""
+"""app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'"""
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://TQ_Admin:TruthQuest24@localhost/TruthQuest_db?auth_plugin=mysql_native_password'
 SCHEDULER_API_ENABLED = True
 
 db = SQLAlchemy(app)
